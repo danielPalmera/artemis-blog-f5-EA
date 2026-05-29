d7aba035c68f753c83feb359999f25a42283989f
Ada <aflorido266@gmail.com>
2026-05-29 14:56:08 +0200

    add style Tailwind footer

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,8 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://danielmolina956.42web.io',
-    base: '/artemis',                     
+  site: 'https://danielmolina956.42web.io',
+
+  base: '/artemis',
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
+
+
