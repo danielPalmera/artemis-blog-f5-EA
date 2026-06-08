eeae3fb6ae0e0732fa7d814d6eab5225f8ac5f50
danielPalmera <danielmolina956@gmail.com>
2026-06-08 11:31:24 +0200

    fix: update fetch URL in getPosts function to use environment variable for API base

--- frontend/src/utils/postUtilss.ts
+++ frontend/src/utils/postUtilss.ts
@@ -2,7 +2,7 @@ import type { Post } from "../types/post";
 
 export async function getPosts(): Promise<Post[]> {
   try {
-    const res = await fetch("http://localhost:3000/api/v1/posts");
+    const res = await fetch(import.meta.env.PUBLIC_API_BASE + "/api/v1/posts");
     if (!res.ok) {
       throw new Error(`Error al obtener los posts: ${res.status} ${res.statusText}`);
     }
