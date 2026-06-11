3d324998f48446db6cad6abbf09a85e4fb316946
aflorido266 <aflorido266@gmail.com>
2026-06-11 15:10:11 +0200

    add variant PreTitle

--- frontend/src/utils/variants.ts
+++ frontend/src/utils/variants.ts
@@ -1,10 +1,14 @@
-
-
-export type H3Variants = "footer"|"related";
+export type H3Variants = "footer" | "related";
 
 export const h3Variants: Record<H3Variants, string> = {
   footer: "font-bold",
   related: "font-normal lg:font-bold leading-snug lg:tracking-tight",
 };
 
+export type PreTitleVariants = "hero"|"related";
+
+export const preTitleVariants: Record<PreTitleVariants, string> = {
+  hero: "",
+  related: "tracking-widest",
+};
 
